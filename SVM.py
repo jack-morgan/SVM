6f57c415669d8bd4aaf68fa5e254de2dfeba46b7
jackmorgan <morgan.jack22@gmail.com>
2017-10-16 17:16:45 +0100

    initial commit

---
+++ SVM.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cvxopt
+import cvxopt.solvers
+from numpy import linalg
+
+
+class SVM(object):
+
+    print('hello')
+#Parameters:
+
+# Kernel: The type of kernel to be used for the SVM
+# C: The penalty parameter for the error term (optional)
+#
+
+def __init__(self, kernel=linear_kernel, C=None):
+    self.kernel = kernel
+    self.C = C
+    if self.C is not None: self.C = float(self.C)
+
+
